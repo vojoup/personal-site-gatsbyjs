639829cb2f86dca5f9eeb103667bc199dc515702
Vojta Oupicky <voj.oup@gmail.com>
2018-12-26 21:34:36 +0100

    Smaller credit text for svg

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -73,20 +73,20 @@ const Footer = () => (
           logo={GatsbyLogo}
           alt="Gatsby Logo"
         />
-        <div>
-          Icons made by
-          {' '}
-          <a href="https://www.freepik.com/" title="Freepik">Freepik</a>
-          {' '}
-          from
-          {' '}
-          <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-          {' '}
-          is licensed by
-          {' '}
-          <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
-        </div>
       </Flex>
+      <div style={{ fontSize: '.4rem' }}>
+        Icons made by
+        {' '}
+        <a href="https://www.freepik.com/" title="Freepik">Freepik</a>
+        {' '}
+        from
+        {' '}
+        <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+        {' '}
+        is licensed by
+        {' '}
+        <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+      </div>
     </Fade>
   </FooterContainer>
 );
